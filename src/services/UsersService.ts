eb014b2a59263aa99ea12418b3857ce5a5b97ea9
Patrick Vinícius Estevão <tonim2612@gmail.com>
2021-04-22 15:56:15 -0300

    Third day

---
+++ src/services/UsersService.ts
@@ -0,0 +1,26 @@
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User"
+import { UsersRepository } from "../repositories/UsersRepository"
+
+class UsersService {
+  private usersRepository: Repository<User>
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository)
+  }
+  async create(email: string): Promise<User> {
+    const userExists = await this.usersRepository.findOne({ email })
+
+    if(userExists) {
+      throw new Error("User already exists!")
+    }
+
+    const user = this.usersRepository.create({ email })
+
+    await this.usersRepository.save(user)
+
+    return user;
+  }
+}
+
+export { UsersService }
