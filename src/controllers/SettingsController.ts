a066d43fbec18db90c68360c27cbbeff6469d97b
Patrick Vinícius Estevão <tonim2612@gmail.com>
2021-04-23 13:12:32 -0300

    4° day

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -16,7 +16,26 @@ class SettingsController {
     } catch(e) {
       return response.status(400).json({ message: e.message })
     }
+  }
+
+  async findByUsername(request: Request, response: Response) {
+    const { username } = request.params
+
+    const settingsService = new SettingsService()
+
+    const settings = await settingsService.findByUsername(username)
+
+    return response.json(settings)
+  }
+
+  async update(request: Request, response: Response) {
+    const { username } = request.params
+    const { chat } = request.body
     
+    const settingsService = new SettingsService()
+
+    const settings = await settingsService.update(username, chat)
+    return response.json(settings)
   }
 }
 
